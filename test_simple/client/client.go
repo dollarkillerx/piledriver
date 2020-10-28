2981bf89d757356f3e3042a8995a104a36f89b6b
dollarkillerx <adapawang@gmail.com>
2020-10-28 08:02:52 -0500

    add pc

--- test_simple/client/client.go
+++ test_simple/client/client.go
@@ -1,1 +1,25 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"plumber/test_simple/proto"
+)
+
+func main() {
+	// 创建一个tcp拨号
+	conn, e := grpc.Dial(":9001", grpc.WithInsecure()) // grpc.WithInsecure() 不安全的传输
+	if e != nil {
+		panic(e.Error())
+	}
+	client := proto.NewSimpleServerClient(conn)
+	simple, err := client.Simple(context.TODO(), &proto.Request{
+		Msg: "hello world",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(simple)
+}
--- test_simple/server/main.go
+++ test_simple/server/main.go
@@ -1,1 +1,41 @@
-package server
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"plumber/test_simple/proto"
+)
+
+func main() {
+	// 创建一个tcp服务
+	addr := "0.0.0.0:9001"
+	listener, e := net.Listen("tcp", addr)
+	if e != nil {
+		panic(e.Error())
+	}
+
+	// 创建一个没有注册的grpc
+	srv := grpc.NewServer()
+	proto.RegisterSimpleServerServer(srv, &server{}) // 注册上去
+	reflection.Register(srv)                         // 注册在给定的gRPC服务器上注册服务器反射服务
+
+	// 监听
+	log.Println("Listen :", addr)
+	if e := srv.Serve(listener); e != nil {
+		panic(e.Error())
+	}
+}
+
+type server struct{}
+
+func (s *server) Simple(ctx context.Context, req *proto.Request) (resp *proto.Response, err error) {
+	log.Println(req.Msg)
+
+	return &proto.Response{
+		Msg: req.Msg + "Spc",
+	}, nil
+}
