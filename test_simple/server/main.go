407eb4b55f8a4c8ae538bdbbdd8f3cf8508219c6
dollarkiller <wangy@mvalley.io>
2020-10-29 17:59:28 +0800

    abc

--- test_simple/server/main.go
+++ test_simple/server/main.go
@@ -1,1 +1,35 @@
-package server
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"plumber/test_simple/proto"
+)
+
+func main() {
+	log.SetFlags(log.Llongfile | log.LstdFlags)
+
+	addr := "0.0.0.0:443"
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer listen.Close()
+
+	srv := grpc.NewServer()
+	proto.RegisterSimpleServerServer(srv,&server{})
+	reflection.Register(srv)
+}
+
+type server struct {
+}
+
+func (s *server) Simple(ctx context.Context, req *proto.Request) (resp *proto.Response, err error) {
+	log.Println(req.Msg)
+
+	return &proto.Response{Msg: req.Msg + "hello"}, nil
+}
